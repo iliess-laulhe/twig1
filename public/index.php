887bd43930f222dba267d35100da87a4ef627203
iliess laulhe <iliesslaulhe@icloud.com>
2021-10-20 11:12:02 +0200

    twig 1 finish

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
-
+require_once __DIR__ . '/../src/products.php';
 // Get a $twig object from this file.
-require_once __DIR__ . '/../config/twig.php';
+require '../config/twig.php';
 
 
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
+
